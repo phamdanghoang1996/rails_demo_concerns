0456af1e940a2a4f26d6488891a79277af2e0782
Hoang Pham <hoangpd@atmarkcafe.org>
2017-10-05 14:08:12 +0700

    When I dont use concern

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,9 @@
 class Comment < ApplicationRecord
-  
+  belongs_to :posts
+
+  validates :infomation, presence: true
+  # Neu lam nhu vay! 2 cai vote o 2 ben! No se bi trong cheo code
+  def vote!
+    vote.create
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,10 @@
 class Post < ApplicationRecord
+  validates :title, :infomation, presence: true
+
+  has_many :votes
+  has_many :comments
+  #Doan nay! Co nghi la: Moi nguoi ta view den cai day se duoc tao 1 cai vote cho bai do! Truong hop dat ra! Nguoi ta vote bai, nhung phai binh luan co
+  def vote!
+    votes.create
+  end
 end
